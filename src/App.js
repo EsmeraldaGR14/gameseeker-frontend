7e67a5c0c360afaf647b3b32c8a3b8e627a43637
Irfan Hanjra <ihanjra@gmail.com>
2023-10-31 03:04:20 -0400

    Added redirects and components folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Spinner from "./components/common/Spinner/Spinner";
 
 function App() {
   return <div className="App"></div>;
