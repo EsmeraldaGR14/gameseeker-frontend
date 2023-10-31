fc835ff569345061db2aaee6012293b667d49603
Hillary Brito <hillarybrito@Hillarys-MacBook-Air.local>
2023-10-31 11:09:27 -0400

    Created components files and navbar.js .css files and imported into the App.js, built out the navbar component and added minor css need to still create the searchbar component that is integrated into the navbar and create a usestate to change the login signup navlink depending on if user has an account and is signed in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router }  from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 function App() {
