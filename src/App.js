056293715079dc336da7c9e99973eaf53b80c5b1
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air.lan>
2023-11-06 11:00:09 -0500

    fixed minor error of the spinner coming from the wrong folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+/* eslint-disable no-unused-vars */
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Spinner from "./components/common/Spinner/Spinner";
+import Spinner from "./utilities/common/Spinner/Spinner";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 function App() {
-
-  return <div className="App">
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-      </Routes>
-    </Router>
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
