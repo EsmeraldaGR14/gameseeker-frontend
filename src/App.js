a5d783ccb3f4abdaa98f44c765d3d6b6cef4593b
Irfan Hanjra <ihanjra@gmail.com>
2023-11-06 11:24:46 -0500

    Pulled carousel component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Spinner from "./components/common/Spinner/Spinner";
+import Spinner from "./utilities/common/Spinner/Spinner";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 function App() {
