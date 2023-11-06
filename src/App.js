7bd5d110e7b5ba0e190b9a360b7694f62b09aab9
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air.lan>
2023-11-06 11:28:10 -0500

    spinner was changed from components to utilities.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
+/* eslint-disable no-unused-vars */
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Spinner from "./components/common/Spinner/Spinner";
+import Spinner from "./utilities/common/Spinner/Spinner";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
 
-  return <div className="App">
-    <Router>
-      <Navbar/>
-    
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-      </Routes>
-  
-    </Router>
-  </div>;
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
