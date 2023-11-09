90eae197dca869ba7ea6b4f6efb3665c10e44726
Irfan Hanjra <ihanjra@gmail.com>
2023-11-09 10:57:18 -0500

    Disable eslint errors

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAllGames } from "../../utilities/Api/Games";
