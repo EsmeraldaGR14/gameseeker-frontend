9a588a0a7ff686f55568b9c57261afea980c90e6
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-11-10 09:59:45 -0500

    basic css done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Spinner from "./components/common/Spinner/Spinner";
+import GameDetails from "./components/GameDetails/GameDetails";
+//import Spinner from "./components/common/Spinner/Spinner";
 import Navbar from "./components/Navbar/Navbar";
 function App() {
   return <div className="App">
     <Router>
       <Navbar/>
+      <Routes>
+        <Route path="/games/:id" element={<GameDetails/>}/>
+      </Routes>
     </Router>
   </div>;
 }
