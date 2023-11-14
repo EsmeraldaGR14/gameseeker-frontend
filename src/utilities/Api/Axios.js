791200afbf75160d3ea9a4ba4c5203dac76287e1
Irfan Hanjra <ihanjra@gmail.com>
2023-11-14 02:39:37 -0500

    Changed database address

--- src/utilities/Api/Axios.js
+++ src/utilities/Api/Axios.js
@@ -4,8 +4,16 @@ const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
-      : "https://gameseeker.onrender.com/",
+      : "https://gameseeker-2.onrender.com",
   timeout: 50000,
 });
+
+AxiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("Axios error:", error);
+    return Promise.reject(error);
+  }
+);
 
 export default AxiosInstance;
