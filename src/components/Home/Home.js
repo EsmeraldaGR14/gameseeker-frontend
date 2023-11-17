43480cdd4cea52cc7f956f9229153cd70c990154
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air.lan>
2023-11-17 00:18:30 -0500

    removed boxart component from home file since i was using it for visual aid.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Carousel1 from "../Carousel/Carousel";
-import BoxArt from "../BoxArt/BoxArt";
 function Home() {
   return (
     <div class="homepage">
-      <BoxArt />
       <Carousel1 />
     </div>
   );
