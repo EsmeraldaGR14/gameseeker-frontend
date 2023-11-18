78f13ea11bbdb67cf25467a5a824d9720ece2b09
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air.lan>
2023-11-18 05:57:55 -0500

    made the next button show in the beginning and not in the last bunch, vice versa for the back button.

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getXGamesAtATime } from "../../utilities/Api/Games";
+import { getXGamesAtATime, getAllGames } from "../../utilities/Api/Games";
 
 /*
  ** should show a range of 25 games in a page
@@ -11,10 +11,24 @@ function Catalog() {
   // I want to make it so that if a user presses next the page will refresh and show the next 25 games and so on and so forth.
 
   const [games, setGames] = useState([]);
+  const [allGamesLength, setAllGamesLength] = useState(0);
   const [limitAndOffset, setLimitAndOffset] = useState({
     limit: 25,
     offset: 0,
   });
+  // const [testing, setTesting] = useState(true);
+
+  useEffect(() => {
+    (async function fetchAllGames() {
+      try {
+        let response = await getAllGames();
+        setAllGamesLength(response.length);
+        console.log("allGamesLength:", response.length);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
   useEffect(() => {
     (async function getAllGamesForTheCatalog() {
@@ -22,41 +36,56 @@ function Catalog() {
         let response = await getXGamesAtATime(limitAndOffset);
 
         console.log(
-          "getAllGamesForTheCatalog:",
-          response.length,
-          response,
-          limitAndOffset
+          // "getAllGamesForTheCatalog:",
+          // response,
+          "limitAndOffset:",
+          limitAndOffset.offset
         );
+        setGames(response);
       } catch (error) {
         console.log(error);
       }
     })();
   }, [limitAndOffset]);
-
+  // console.log(games[0]);
   return (
     <div>
-      <button
-        onClick={() => {
-          let newOffset = limitAndOffset.offset - 25;
-          setLimitAndOffset((prevState) => ({
-            ...prevState,
-            offset: newOffset,
-          }));
-        }}
-      >
-        BACK
-      </button>
-      <button
-        onClick={() => {
-          let newOffset = limitAndOffset.offset + 25;
-          setLimitAndOffset((prevState) => ({
-            ...prevState,
-            offset: newOffset,
-          }));
-        }}
-      >
-        NEXT
-      </button>
+      {/* if we are passed the first bunch
+       **
+       */}
+      {limitAndOffset.offset > 0 && (
+        <button
+          onClick={() => {
+            let newOffset = limitAndOffset.offset - 25;
+            setLimitAndOffset((prevState) => ({
+              ...prevState,
+              offset: newOffset,
+            }));
+          }}
+        >
+          BACK
+        </button>
+      )}
+      <div>
+        {games.map((game) => (
+          <div key={game.id}>{game.id}</div>
+        ))}
+      </div>
+      {/* once it reaches the last bunch of the array that can be fetched next button should not show */}
+      {games.length > 0 &&
+        allGamesLength - limitAndOffset.offset > limitAndOffset.limit && (
+          <button
+            onClick={() => {
+              let newOffset = limitAndOffset.offset + 25;
+              setLimitAndOffset((prevState) => ({
+                ...prevState,
+                offset: newOffset,
+              }));
+            }}
+          >
+            NEXT
+          </button>
+        )}
     </div>
   );
 }
