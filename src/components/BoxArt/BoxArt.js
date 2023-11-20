1acbdb284ee82c216765d561901a31aff67c182e
Irfan Hanjra <ihanjra@gmail.com>
2023-11-20 13:13:35 -0500

    Styled search results page

--- src/components/BoxArt/BoxArt.js
+++ src/components/BoxArt/BoxArt.js
@@ -6,11 +6,28 @@ import "./BoxArt.css";
  ** HEIGHT FOR IMAGES? 300PX
  */
 
-function BoxArt({ image, name, year }) {
+function BoxArt({ image, name, year, className }) {
+  const handleMouseMove = (e) => {
+    const card = e.currentTarget;
+    const boundingBox = card.getBoundingClientRect();
+    const mouseX = e.clientX - boundingBox.left;
+    const mouseY = e.clientY - boundingBox.top;
+    const rotateX = (mouseY / boundingBox.height - 0.5) * 30;
+    const rotateY = (mouseX / boundingBox.width - 0.5) * 30;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  };
+
+  const handleMouseLeave = (e) => {
+    e.currentTarget.style.transform = "rotateX(0) rotateY(0)";
+  };
   return (
-    <div class="card">
-      <img src={image} alt="" height="300px"></img>
-      <div>{name}</div>
+    <div
+      className={`card-${className}`}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img src={image} alt={name}></img>
     </div>
   );
 }
--- src/components/SearchResultsOverlay/SearchResultsOverlay.js
+++ src/components/SearchResultsOverlay/SearchResultsOverlay.js
@@ -2,32 +2,37 @@ import "./SearchResultsOverlay.css";
 import { Link } from "react-router-dom";
 import { extractYear } from "../../utilities/helpers/extractYear";
 import React from "react";
+import BoxArt from "../BoxArt/BoxArt";
 
 const SearchResultsOverlay = ({ searchResults, overlayVisible }) => {
   return (
     <>
-    {overlayVisible && (
-        <div className="overlay-background"></div>
-      )}
-    <div className={`search-results-overlay ${overlayVisible ? "visible" : ""}`}>
-      {searchResults.length > 0 && (
-        <ul className="search-results-list">
-          {searchResults.map((result) => (
-            <Link to={`/games/${result.id}`} key={result.id}>
-              <li className="search-results-item">
-                <img
+      {overlayVisible && <div className="overlay-background"></div>}
+      <div
+        className={`search-results-overlay ${overlayVisible ? "visible" : ""}`}
+      >
+        {searchResults.length > 0 && (
+          <ul className="search-results-list">
+            {searchResults.map((result) => (
+              <Link to={`/games/${result.id}`} key={result.id}>
+                <li className="search-results-item">
+                  <BoxArt
+                    image={result.boxart}
+                    year={extractYear(result.release_date)}
+                  />
+                  {/* <img
                   className="boxart"
                   style={{ height: "5rem" }}
                   src={result.boxart}
                   alt={`${result.title} Box Art`}
-                />
-                {result.title} ({extractYear(result.release_date)})
-              </li>
-            </Link>
-          ))}
-        </ul>
-      )}
-    </div>
+                /> */}
+                  {result.title} ({extractYear(result.release_date)})
+                </li>
+              </Link>
+            ))}
+          </ul>
+        )}
+      </div>
     </>
   );
 };
