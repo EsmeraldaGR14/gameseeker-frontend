18fa9cc5e1eca864341d43a1b3508411aa8b9503
Irfan Hanjra <ihanjra@gmail.com>
2023-11-29 02:26:31 -0500

    Fixed eslint throw error

--- src/utilities/Api/Collection.js
+++ src/utilities/Api/Collection.js
@@ -6,7 +6,7 @@ async function getGameCollection(userId) {
     return response.data;
   } catch (error) {
     console.error("Error fetching collection", error);
-    throw { message: "Error fetching collection. Please try again later." };
+    throw new Error("Error fetching collection. Please try again later.");
   }
 }
 
@@ -16,7 +16,7 @@ async function addGameToCollection(userId, gameId) {
     return response.data;
   } catch (error) {
     console.error("Error adding game to collection", error);
-    throw { message: "Error adding game to collection. Please try again later." };
+    throw new Error ("Error adding game to collection. Please try again later.");
   }
 }
 
@@ -26,7 +26,7 @@ async function deleteGameFromCollection(userId, gameId) {
     return response.data;
   } catch (error) {
     console.error("Error deleting game from collection", error);
-    throw { message: "Error deleting game from collection. Please try again later." };
+    throw new Error ("Error deleting game from collection. Please try again later.");
   }
 }
 
