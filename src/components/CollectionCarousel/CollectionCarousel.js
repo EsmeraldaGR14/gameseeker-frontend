4013e4150eb7bba33c1f861118fd198327592c48
Hillary <hillarybrito@pursuit.org>
2023-12-14 05:45:22 -0500

    Carousels completed and refactored home page to be centered with welcome message

---
+++ src/components/CollectionCarousel/CollectionCarousel.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState, useCallback } from "react";
+import GenericCarousel from "../Carousel/Carousel";
+import { useUser } from "../UserContext";
+import { getGameCollection } from "../../utilities/Api/Collection";
+
+function CollectionCarousel() {
+  const { user } = useUser();
+  const [collectionData, setCollectionData] = useState([]);
+
+  const getCollectionById = useCallback(async () => {
+    try {
+      let result = await getGameCollection(user.id);
+      setCollectionData(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [user.id]);
+
+  useEffect(() => {
+    getCollectionById();
+  }, [getCollectionById]);
+
+  return <GenericCarousel label="Collection" items={collectionData} />;
+}
+
+export default CollectionCarousel;
