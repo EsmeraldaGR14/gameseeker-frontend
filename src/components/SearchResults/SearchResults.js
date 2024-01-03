14509e9586a874b5dfc33d8c585ac238830d752f
Irfan Hanjra <ihanjra@gmail.com>
2024-01-03 14:08:47 -0500

    Added filtering logic to search results page

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -7,7 +7,20 @@ import ScrollButton from "../../utilities/common/ScrollButton/ScrollButton";
 import { extractYear } from "../../utilities/helpers/extractYear";
 import BoxArt from "../BoxArt/BoxArt";
 import Modal from "../../utilities/common/Modal/Modal";
-import SortingandFilteringButtons from "../../utilities/common/SortingandFilteringButtons/SortingandFilteringButtons";
+// import SortingandFilteringButtons from "../../utilities/common/SortingandFilteringButtons/SortingandFilteringButtons";
+
+const subscriptionServices = [
+  "PlayStation Plus Essential",
+  "PlayStation Plus Extra",
+  "PlayStation Plus Premium",
+  "Xbox Game Pass Core",
+  "Xbox Game Pass",
+  "PC Game Pass",
+  "GeForce Now",
+  "Nintendo Switch Online",
+  "Ubisoft+",
+  "Apple Arcade",
+];
 
 function SearchResultsPage() {
   const [searchResults, setSearchResults] = useState([]);
@@ -16,6 +29,9 @@ function SearchResultsPage() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const searchQuery = searchParams.get("query");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedServices, setSelectedServices] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
 
   const handleSearch = async (query) => {
     try {
@@ -24,13 +40,52 @@ function SearchResultsPage() {
         game.title.toLowerCase().includes(query.toLowerCase())
       );
       setSearchResults(filteredGames || []);
-      console.log(searchResults)
     } catch (error) {
       console.error("Error fetching search results:", error);
       setSearchResults([]);
     }
   };
 
+  const handleFilter = () => {
+    try {
+      let filteredGames = [...searchResults];
+      if (selectedServices.length > 0) {
+        filteredGames = filteredGames.filter(
+          (game) =>
+            Array.isArray(game.subscription) &&
+            game.subscription.some((sub) => selectedServices.includes(sub))
+        );
+      }
+
+      setFilteredResults(filteredGames);
+      console.log(filteredGames);
+    } catch (error) {
+      console.error("Error filtering games:", error);
+    }
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  const handleServiceToggle = (service) => {
+    const updatedServices = selectedServices.includes(service)
+      ? selectedServices.filter((s) => s !== service)
+      : [...selectedServices, service];
+
+    setSelectedServices(updatedServices);
+    console.log(
+      `Checkbox for service "${service}" clicked. Updated services:`,
+      updatedServices
+    );
+    //  console.log(`originalGames ${[originalGames]}`);
+    console.log(`Games ${searchResults}`);
+  };
+
+  useEffect(() => {
+    handleFilter();
+  }, [selectedServices]);
+  
   // const handleSort = (criteria) => {
   //   setSortCriteria(criteria);
   //   setSelectedSortCriteria(criteria);
@@ -61,33 +116,87 @@ function SearchResultsPage() {
 
   return (
     <div className="container">
-      <SortingandFilteringButtons
-        games={searchResults}
+      {/* <SortingandFilteringButtons
+        sortedGames={searchResults}
         setSortedGames={setSearchResults}
-      />
+        filteredGames={searchResults}
+        setFilteredGames={setFilteredResults}
+      /> */}
+      <div className="subscription-filter">
+        <div className="dropdown">
+          <div className="dropdown-title" onClick={toggleDropdown}>
+            <span>
+              {selectedServices.length === 0
+                ? "Select Services"
+                : selectedServices.join(", ")}
+            </span>
+            <span className={`arrow ${isDropdownOpen ? "up" : "down"}`}>
+              {isDropdownOpen ? "\u25B2" : "\u25BC"}
+            </span>
+          </div>
+          {isDropdownOpen && (
+            <div className="dropdown-options">
+              {subscriptionServices.map((service) => (
+                <div
+                  key={service}
+                  className="option"
+                  onClick={() => handleServiceToggle(service)}
+                >
+                  <input
+                    type="checkbox"
+                    checked={selectedServices.includes(service)}
+                    onChange={() => {}}
+                  />
+                  {service}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
       <div className="search-results grid-view">
         <div className={`search-results-title`}>
           <h1>Search Results</h1>
-          {searchResults.length > 0 ? (
+          {filteredResults.length > 0 ? (
             <p>
-              {searchResults.length} result(s) found for "{searchQuery}":
+              {filteredResults.length} result(s) found for "{searchQuery}":
             </p>
           ) : (
             <p>Sorry, there are no results.</p>
           )}
         </div>
         <div className="search-results grid-view">
-          {searchResults.map((game) => (
-            <Link to={`/games/${game.id}`} className="item-link" key={game.id}>
-              <BoxArt
-                className="boxart"
-                image={game.boxart}
-                name={game.title}
-                year={extractYear(game.release_date)}
-                openModal={openModal}
-              />
-            </Link>
-          ))}
+          {filteredResults.length > 0
+            ? filteredResults.map((game) => (
+                <Link
+                  to={`/games/${game.id}`}
+                  className="item-link"
+                  key={game.id}
+                >
+                  <BoxArt
+                    className="boxart"
+                    image={game.boxart}
+                    name={game.title}
+                    year={extractYear(game.release_date)}
+                    openModal={openModal}
+                  />
+                </Link>
+              ))
+            : searchResults.map((game) => (
+                <Link
+                  to={`/games/${game.id}`}
+                  className="item-link"
+                  key={game.id}
+                >
+                  <BoxArt
+                    className="boxart"
+                    image={game.boxart}
+                    name={game.title}
+                    year={extractYear(game.release_date)}
+                    openModal={openModal}
+                  />
+                </Link>
+              ))}
           <ScrollButton />
           <div className="search-modal-id">
             {isModalOpen && (
--- src/utilities/common/SortingandFilteringButtons/SortingandFilteringButtons.js
+++ src/utilities/common/SortingandFilteringButtons/SortingandFilteringButtons.js
@@ -2,79 +2,209 @@ import React, { useState, useCallback, useEffect } from "react";
 import { extractYear } from "../../helpers/extractYear";
 import "./SortingandFilteringButtons.css"
 
-function SortingandFilteringButtons({ games, setSortedGames }) {
-  const [sortCriteria, setSortCriteria] = useState("relevance");
-  const [selectedSortCriteria, setSelectedSortCriteria] = useState("relevance");
+const subscriptionServices = [
+  "PlayStation Plus Essential",
+  "PlayStation Plus Extra",
+  "PlayStation Plus Premium",
+  "Xbox Game Pass Core",
+  "Xbox Game Pass",
+  "PC Game Pass",
+  "GeForce Now",
+  "Nintendo Switch Online",
+  "Ubisoft+",
+  "Apple Arcade",
+];
 
-  const removeTheFromTitle = (title) => {
-    const normalizedTitle = title.toLowerCase();
-    return normalizedTitle.startsWith("the ")
-      ? normalizedTitle.slice(4)
-      : normalizedTitle;
-  };
+function SortingandFilteringButtons({ sortedGames, setSortedGames }) {
+  // const [sortCriteria, setSortCriteria] = useState("relevance");
+  // const [selectedSortCriteria, setSelectedSortCriteria] = useState("relevance");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedServices, setSelectedServices] = useState([]);
 
-   const handleSortAndFilter = (criteria) => {
-     setSortCriteria(criteria);
-     setSelectedSortCriteria(criteria);
+  // const removeTheFromTitle = (title) => {
+  //   const normalizedTitle = title.toLowerCase();
+  //   return normalizedTitle.startsWith("the ")
+  //     ? normalizedTitle.slice(4)
+  //     : normalizedTitle;
+  // };
+
+  //  const handleCriteria = (criteria) => {
+  //    setSortCriteria(criteria);
+  //    setSelectedSortCriteria(criteria);
+  //  };
+
+   const toggleDropdown = () => {
+     setIsDropdownOpen((prev) => !prev);
    };
 
-   const handleSort = useCallback(() => {
-     try {
-       let sortedGames = [...games];
+   const handleServiceToggle = (service) => {
+     const updatedServices = selectedServices.includes(service)
+       ? selectedServices.filter((s) => s !== service)
+       : [...selectedServices, service];
 
-       if (sortCriteria === "title") {
-         console.log("Sorting by title");
-         sortedGames.sort((a, b) =>
-           removeTheFromTitle(a.title).localeCompare(
-             removeTheFromTitle(b.title)
-           )
+     setSelectedServices(updatedServices);
+     console.log(
+       `Checkbox for service "${service}" clicked. Updated services:`,
+       updatedServices
+     );
+    //  console.log(`originalGames ${[originalGames]}`);
+     console.log(`Games ${sortedGames}`);
+   };
+
+
+  //  const handleSort = useCallback(() => {
+  //    try {
+  //      let sortedGames = [...games];
+
+  //     //  if (selectedServices.length > 0) {
+  //     //    sortedGames = sortedGames.filter(
+  //     //      (game) =>
+  //     //        Array.isArray(game.subscription) &&
+  //     //        game.subscription.some((sub) => selectedServices.includes(sub))
+  //     //    );
+  //     //  }
+
+  //      if (sortCriteria === "title") {
+  //        console.log("Sorting by title");
+  //        sortedGames.sort((a, b) =>
+  //          removeTheFromTitle(a.title).localeCompare(
+  //            removeTheFromTitle(b.title)
+  //          )
+  //        );
+  //      } else if (sortCriteria === "releaseDate") {
+  //        console.log("Sorting by date");
+  //        sortedGames.sort((a, b) =>
+  //          extractYear(a.release_date)
+  //            .toString()
+  //            .localeCompare(extractYear(b.release_date).toString())
+  //        );
+  //      }
+  //      setSortedGames(sortedGames);
+  //    } catch (error) {
+  //      console.error("Error sorting games:", error);
+  //    }
+  //  }, [games, sortCriteria, setSortedGames]);
+
+   const handleFilter = () => {
+     try {
+       let filteredGames = [...sortedGames];
+
+       if (selectedServices.length > 0) {
+         filteredGames = filteredGames.filter(
+           (game) =>
+             Array.isArray(game.subscription) &&
+             game.subscription.some((sub) => selectedServices.includes(sub))
          );
-       } else if (sortCriteria === "releaseDate") {
-         console.log("Sorting by date");
-         sortedGames.sort((a, b) =>
-           extractYear(a.release_date)
-             .toString()
-             .localeCompare(extractYear(b.release_date).toString())
-         );
+       } else {
+         filteredGames = [...sortedGames];
        }
-       setSortedGames(sortedGames);
-       console.log(sortedGames);
+       setSortedGames(filteredGames);
+       console.log(filteredGames);
      } catch (error) {
-       console.error("Error sorting games:", error);
+       console.error("Error filtering games:", error);
      }
-   }, [games, sortCriteria, setSortedGames]);
+   };
+
+  //  useEffect(() => {
+  //    handleSort();
+  //  }, [sortCriteria]);
 
    useEffect(() => {
-     handleSort();
-   }, [sortCriteria]);
+     handleFilter();
+   }, [selectedServices]);
+
+  //  useEffect(() => {
+  //    setOriginalGames(games);
+  //  }, [games]);
 
   return (
     <div className="container">
       <div className="sort-buttons">
         <button
           type="button"
-          onClick={() => handleSortAndFilter("relevance")}
-          className={selectedSortCriteria === "relevance" ? "selected" : ""}
+          // onClick={() => handleCriteria("relevance")}
+          // className={selectedSortCriteria === "relevance" ? "selected" : ""}
         >
           Relevance
         </button>
         <button
           type="button"
-          onClick={() => handleSortAndFilter("title")}
-          className={selectedSortCriteria === "title" ? "selected" : ""}
+          // onClick={() => handleCriteria("title")}
+          // className={selectedSortCriteria === "title" ? "selected" : ""}
         >
           Title
         </button>
         <button
           type="button"
-          onClick={() => handleSortAndFilter("releaseDate")}
-          className={selectedSortCriteria === "releaseDate" ? "selected" : ""}
+          // onClick={() => handleCriteria("releaseDate")}
+          // className={selectedSortCriteria === "releaseDate" ? "selected" : ""}
         >
           Release Date
         </button>
+      </div>
+      <div className="subscription-filter">
+        <div className="dropdown">
+          <div className="dropdown-title" onClick={toggleDropdown}>
+            <span>
+              {selectedServices.length === 0
+                ? "Select Services"
+                : selectedServices.join(", ")}
+            </span>
+            <span className={`arrow ${isDropdownOpen ? "up" : "down"}`}>
+              {isDropdownOpen ? "\u25B2" : "\u25BC"}
+            </span>
+          </div>
+          {isDropdownOpen && (
+            <div className="dropdown-options">
+              {subscriptionServices.map((service) => (
+                <div
+                  key={service}
+                  className="option"
+                  onClick={() => handleServiceToggle(service)}
+                >
+                  <input
+                    type="checkbox"
+                    checked={selectedServices.includes(service)}
+                    onChange={() => {}}
+                  />
+                  {service}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
 export default SortingandFilteringButtons;
+
+// UPDATE game
+// SET subscription = CASE
+//     WHEN id = 4 THEN ARRAY['Xbox Game Pass',
+//   'PC Game Pass']
+//     WHEN id = 5 THEN ARRAY['Ubisoft+',
+//   'Apple Arcade']
+//     WHEN id = 6 THEN ARRAY['GeForce Now',
+//   'Nintendo Switch Online']
+//     WHEN id = 7 THEN ARRAY[ 'Xbox Game Pass',
+//   'PC Game Pass']
+//     WHEN id = 8 THEN ARRAY['Ubisoft+']
+//     WHEN id = 9 THEN ARRAY['Nintendo Switch Online', 'Ubisoft+']
+//     WHEN id = 10 THEN ARRAY['Nintendo Switch Online']
+//     WHEN id = 11 THEN ARRAY['Nintendo Switch Online', 'Ubisoft+']
+//     ELSE ARRAY[]::VARCHAR[]
+// END
+// WHERE id IN (4, 5, 6, 7, 8, 9, 10, 11);
+
+// 'PlayStation Plus Essential',
+//   'PlayStation Plus Extra",
+//   "PlayStation Plus Premium",
+//   "Xbox Game Pass Core",
+//   "Xbox Game Pass",
+//   "PC Game Pass",
+//   "GeForce Now",
+//   "Nintendo Switch Online",
+//   "Ubisoft+",
+//   "Apple Arcade",
